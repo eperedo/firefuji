b837a6d0e326f85d63a1ed77dabb455e082829e0
Eduardo Peredo <jrperedo@gmail.com>
2017-09-17 16:05:09 -0500

    added fb config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,9 @@
 module.exports = {
-  NODE_ENV: '"production"'
-}
+	NODE_ENV: '"production"',
+	apiKey: '"AIzaSyCPmxdugOlKXlw-ZsZpVh6eVZYjMBCYjmY"',
+	authDomain: '"fujideces.firebaseapp.com"',
+	databaseURL: '"https://fujideces.firebaseio.com"',
+	projectId: '"fujideces"',
+	storageBucket: '"fujideces.appspot.com"',
+	messagingSenderIdd: '"716842806435"',
+};
